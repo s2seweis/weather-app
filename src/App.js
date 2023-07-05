7792ef6349c398c01c0bcf9b9209856f22b50216
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-07-05 09:38:40 +0800

    upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import LocationContextProvider from "./_context/locationContext";
+import CurrentWeatherContextProvider from "./_context/currentWeatherContext";
+import { Typography } from "@material-ui/core";
+import AppContainer from "./container/appContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+
+
+        <  Typography style={{marginTop:"20px", marginBottom:"20px"}} variant='h4'>Weather App
+         <a href="https://github.com/s2seweis" target="_blank">
+            <img style={{ height: '25px', width: '25px', marginLeft: '10px' }}
+              src={require("./assets/github.png")} alt="github" />
+          </a>
+        </Typography>
+
+
+        <LocationContextProvider>
+          <CurrentWeatherContextProvider>
+            <AppContainer />
+          </CurrentWeatherContextProvider>
+        </LocationContextProvider>
+      </div>
+    );
+  }
 }
 
 export default App;
+
